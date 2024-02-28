6f3c3026839329be323282ba52f930a53c843d5d
joe-ton <joeton.public@gmail.com>
2024-02-27 23:05:04 -0800

    Added beeping sound for start/end learning bout

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gen2brain/beeep"
+)
 
 func main() {
-	// Testing sending out Git
-	fmt.Println("Hello, World!")
+	// Beep with default frequency and duration
+	// Used for denoting break sound for studying
+	beeep.Beep(beeep.DefaultFreq, beeep.DefaultDuration)
 }
